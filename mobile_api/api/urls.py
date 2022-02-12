30385b68fcee35bcb56f520c5d9eee3c05712484
Artem Frolov <Artem.Frolov@kaspersky.com>
2022-02-12 23:07:42 +0300

    update

--- mobile_api/api/urls.py
+++ mobile_api/api/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, StoreViewSet, VisitViewSet
 
-router = routers.DefaultRouter()
+router = DefaultRouter()
 router.register(r"users", UserViewSet)
 router.register(
     r"(?P<phone_number>\d+)/stores",
